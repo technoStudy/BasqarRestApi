ea91bff5a629ba3c4d37863d290c2fc8225da8cf
TechnoStudy <info@techno.study>
2020-02-25 20:27:48 -0500

    Notification test

---
+++ src/test/java/NotificationTest.java
@@ -0,0 +1,179 @@
+import io.restassured.http.ContentType;
+import model.Location;
+import model.Notification;
+import model.School;
+import org.testng.annotations.Test;
+import utility.BaseTest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class NotificationTest extends BaseTest {
+    private String apiPath = "/school-service/api/notifications";
+
+    @Test
+    public void createTest() {
+        Notification model = getBody();
+
+        // creating entity
+        String entityId = given()
+                .cookies( cookies )
+                .body( model )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( apiPath )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // deleting entity
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( apiPath + "/" + entityId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    private Notification getBody() {
+        Notification model = new Notification();
+        model.setName( name );
+        model.setDescription( code );
+        model.setType( "UPCOMING_PAYMENT_DEADLINE" );
+        model.setSchoolId( "5e035f8c9ea1a129f71ac585" );
+        return model;
+    }
+
+    @Test
+    public void editTest() {
+        Notification entity = getBody();
+
+        // creating entity
+        String entityId = given()
+                .cookies( cookies )
+                .body( entity )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( apiPath )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // Editing entity
+        entity.setId( entityId );
+        entity.setName( nameEdited );
+        entity.setDescription( codeEdited );
+        given()
+                .cookies( cookies )
+                .body( entity )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .put( apiPath )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+                .body( "name", equalTo( entity.getName() ) )
+                .body( "description", equalTo( entity.getDescription() ) )
+        ;
+
+        // deleting entity
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( apiPath + "/" + entityId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    @Test
+    public void createNegativeTest() {
+        Notification entity = getBody();
+
+        // creating entity
+        String entityId = given()
+                .cookies( cookies )
+                .body( entity )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( apiPath )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // entity creation negative test
+        given()
+                .cookies( cookies )
+                .body( entity )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( apiPath )
+                .then()
+                .log().body()
+                .statusCode( 400 );
+
+        // deleting entity
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( apiPath + "/" + entityId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    @Test
+    public void deleteNegativeTest() {
+        Notification entity = getBody();
+
+        // creating entity
+        String entityId = given()
+                .cookies( cookies )
+                .body( entity )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( apiPath )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // deleting entity
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( apiPath + "/" + entityId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+
+        // deleting entity again
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( apiPath + "/" + entityId )
+                .then()
+                .log().body()
+                .statusCode( 404 )
+        ;
+    }
+}
---
+++ src/test/java/model/Notification.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Notification {
+    private String id;
+    private String name;
+    private String type;
+    private String schoolId;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
